c47f1d42b1a4b12e16ac55f2dc4d7a89aaf79c17
Victor Camargo <victorperis.camargo@gmail.com>
2018-05-28 00:46:44 -0300

    Comentarios adicionados

--- bkp_to_s3.py
+++ bkp_to_s3.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import time
-import subprocess
-import os.path
+import time         # Importa lib time
+import subprocess   # Imposta lib subprocess
+import os.path      # Importa lib os.path
 
 def main():
 
@@ -23,8 +23,8 @@ def main():
     subprocess.call(upload + log, shell=True)   # Chama o subprocesso swiftFile
 
     #Printa o final e relatório
-    diaInicio = (time.strftime("%d-%m-%Y"))
-    final     = fim(diaInicio, horaInicio, backup, pathlog)
+    diaInicio = (time.strftime("%d-%m-%Y"))                    # Cria hora que o backup terminou
+    final     = fim(diaInicio, horaInicio, backup, pathlog)    # Executa funcao final
     r         = open(pathlog, 'a')  # o 'w' abre o arquivo com permissao de escrita, se fosse só pra ler, seria 'r'
     r.write(final)                  # Escreve no arquivo
     r.close()                       # Fecha e salva o arquivo
@@ -70,42 +70,42 @@ def fim(diaInicio, horaInicio, backup, pathlog):
 def geralog():
     date        = (time.strftime("%Y-%m-%d"))         # Cria data
     logfile     = '%s-backup.log' % date              # Cria arquivo de Log
-    pathlog    = '/home/victor/scripts/backup2/logs/' # Define diretório para log
+    pathlog    = '/home/victor/scripts/backup2/logs/' # Define diretorio para log
 
-    if os.path.exists(pathlog):
+    if os.path.exists(pathlog):                       # Verifica se o diretorio existe
         addlogfile = '%s%s' % (pathlog, logfile)      # Arquivo de log
     else:
-        os.mkdir(pathlog)
+        os.mkdir(pathlog)                             # Cria diretorio
         addlogfile = '%s%s' % (pathlog, logfile)      # Arquivo de log
 
     return addlogfile
 
 #CONSTROI O ARQUIVO E PATH DE BACKUP E RETORNA
 def gerabackup():
-    date         = (time.strftime("%Y-%m-%d"))                     # Cria data
-    hora         = (time.strftime('%H-%M-%S'))                     # Cria hora
-    swiftstorage = 's3://backup_sonar_db'                          # Define endereço ObjectStorage
-    pathdestino  = '/home/victor/scripts/backup2/destino/'                   # Define destino onde será gravado o Backup
+    date         = (time.strftime("%Y-%m-%d"))   # Cria data
+    hora         = (time.strftime('%H-%M-%S'))   # Cria hora
+    swiftstorage = 's3://backup_sonar_db'        # Define endereço ObjectStorage
+    pathdestino  = '/home/victor/scripts/backup2/destino/'                   # Define destino onde sera gravado o Backup
     pathorigem   = '/home/victor/scripts/backup2/origem/'                    # Define Pasta que será 'backupeada'
     backupfile   = '%s%s_%s-bkp-pgdump.tar.gz' % (pathdestino, date, hora)   # Define o nome do arquivo de Backup
 
     #dumpfile   = '/opt/rh/postgresql92/root/usr/bin/pg_dump -U sonar > /%s/%s-%s-PGDUMP.sql' % (pathorigem, date, hora) # Gera o dumpfile do banco
     
-    if os.path.exists(pathorigem):
-        dumpfile = '/bin/touch %s%s_%s-PGTESTE.sql' % (pathorigem, date, hora)                       # Gera arquivo de teste
+    if os.path.exists(pathorigem):                                              # Verifica se o diretorio existe
+        dumpfile = '/bin/touch %s%s_%s-PGTESTE.sql' % (pathorigem, date, hora)  # Gera arquivo de teste
     else:
-        os.mkdir(pathorigem)
-        dumpfile = '/bin/touch %s%s_%s-PGTESTE.sql' % (pathorigem, date, hora)                       # Gera arquivo de teste
+        os.mkdir(pathorigem)                                                    # Cria diretorio
+        dumpfile = '/bin/touch %s%s_%s-PGTESTE.sql' % (pathorigem, date, hora)  # Gera arquivo de teste
 
-    if os.path.exists(pathdestino):
+    if os.path.exists(pathdestino):                                                                 # Verifica se o diretorio existe
         backup   = 'tar -P -cvf %s %s%s_%s-PGTESTE.sql' % (backupfile, pathorigem, date, hora)      # Compacta arquivo de dump
         upload   = '/opt/itau_sysadmintools/s3cmd/bin/s3cmd put %s %s' % (backupfile, swiftstorage) # Faz upload para o Object Storage
     else:
-        os.mkdir(pathdestino)
+        os.mkdir(pathdestino)                                                                       # Cria diretorio
         backup   = 'tar -P -cvf %s %s%s_%s-PGTESTE.sql' % (backupfile, pathorigem, date, hora)      # Compacta arquivo de dump
         upload   = '/opt/itau_sysadmintools/s3cmd/bin/s3cmd put %s %s' % (backupfile, swiftstorage) # Faz upload para o Object Storage
 
-    return dumpfile,backup,upload
+    return dumpfile,backup,upload       # Retorna as saidas que serao chamadas no subprocess
 
 #Roda a funcao main no inicio do codigo
 main()
